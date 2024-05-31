a993ce2a499ef647e33b784b7ec0dfbb50ad9d52
Danny Buglak <dannybuglak4@gmail.com>
2024-05-31 17:48:38 -0400

    Push

---
+++ conference/src/main/java/Application.java
@@ -0,0 +1,12 @@
+import com.pluralsight.service.SpeakerServiceImpl;
+import com.pluralsight.service.SpeakerService;
+
+public class Application {
+
+    public static void main (String[] args) {
+        SpeakerService service = new SpeakerServiceImpl();
+
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+
+}
